2afc20113c7ae5c0eecc159fc4865a607b84ffac
Andre Wang on his desktop <andrewang87@yahoo.com.au>
2023-12-29 16:35:28 +1100

    finish lesson 9, making uathorization for routes

---
+++ lesson03/middleware/verifyJWT.js
@@ -0,0 +1,25 @@
+// necessary to do any kind of protection on api.
+
+const jwt = require('jsonwebtoken')
+const verifyJWT = (req, res, next) => {
+    const authHeader = req.headers.authorization || req.headers.Authorization
+
+    if (!authHeader?.startsWith('Bearer ')) {
+        return res.status(401).json({ message: 'Unauthorized' })
+    }
+
+    const token = authHeader.split(' ')[1] // the token code, not with 'Bearer'.
+
+    jwt.verify(
+        token,
+        process.env.ACCESS_TOKEN_SECRET,
+        (err, decoded) => {
+            if (err) return res.status(403).json({ message: 'Forbidden' })
+            req.user = decoded.UserInfo.username
+            req.roles = decoded.UserInfo.roles
+            next() // goes to next middleware, or the controller.
+        }
+    )
+}
+
+module.exports = verifyJWT 
--- lesson03/routes/noteRoutes.js
+++ lesson03/routes/noteRoutes.js
@@ -2,6 +2,10 @@ const express = require('express')
 const router = express.Router()
 const notesController = require('../controllers/notesController')
 
+const verifyJWT = require('../middleware/verifyJWT')
+
+router.use(verifyJWT)// apply middleware to all of it. Can do individually instead
+
 router.route('/') //already at notes because specified in server.py
     .get(notesController.getAllNotes)
     .post(notesController.createNewNote)
