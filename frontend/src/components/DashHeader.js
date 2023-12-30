e05df23442d4c984819f78759716af2b9758d21b
Andre Wang on his desktop <andrewang87@yahoo.com.au>
2023-12-30 16:55:48 +1100

    made the login system, but requests still can't be made after logging in. Lesson 9 done.

--- frontend/src/components/DashHeader.js
+++ frontend/src/components/DashHeader.js
@@ -1,15 +1,79 @@
-import { Link } from 'react-router-dom'
+import { useEffect } from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faRightFromBracket } from "@fortawesome/free-solid-svg-icons"
+import { useNavigate, Link, useLocation } from 'react-router-dom'
+
+import { useSendLogoutMutation } from '../features/auth/authApiSlice'
+
+const DASH_REGEX = /^\/dash(\/)?$/
+const NOTES_REGEX = /^\/dash\/notes(\/)?$/
+const USERS_REGEX = /^\/dash\/users(\/)?$/
 
 const DashHeader = () => {
 
+    const navigate = useNavigate()
+    const { pathname } = useLocation()
+
+    const [sendLogout, {
+        isLoading,
+        isSuccess,
+        isError,
+        error
+    }] = useSendLogoutMutation()
+
+    useEffect(() => {
+        // SEEMS NOT TO WORK.
+        if (isSuccess) {
+            navigate('/') //because its looking for success of a logout.
+            console.log('successful sendlogout status')
+        }
+    }, [isSuccess, navigate])
+
+    if (isLoading) {
+        console.log('loading logout request')
+        return <p>Logging Out...</p>
+    }
+
+    if (isError) {
+        console.log('errored logout request')
+        return <p>Error: {error.data?.message}</p>
+    }
+
+    let dashClass = null 
+    //check not on dash, notes list or users list.
+    if (!DASH_REGEX.test(pathname) && !NOTES_REGEX.test(pathname) && !USERS_REGEX.test(pathname)) {
+        dashClass = "dash-header__container--small"
+    }
+
+    const onLogoutClicked = async () => {
+        // MY FIX FOR LOGGING OUT THEN NAVIGATING.
+        try {
+            await sendLogout().unwrap()
+            navigate('/')
+        } catch {
+            console.error('logout error')
+        }
+    }
+
+    const logoutButton = (
+        <button
+            className="icon-button"
+            title="Logout"
+            onClick={onLogoutClicked}
+        >
+            <FontAwesomeIcon icon={faRightFromBracket} />
+        </button>
+    )
+
     const content = (
         <header className="dash-header">
-            <div className="dash-header__container">
+            <div className={`dash-header__container ${dashClass}`}>
                 <Link to="/dash">
                     <h1 className="dash-header__title">techNotes</h1>
                 </Link>
                 <nav className="dash-header__nav">
-                    {/* add nav buttons later */}
+                    {/* add more buttons later */}
+                    {logoutButton}
                 </nav>
             </div>
         </header>
---
+++ frontend/src/features/auth/authSlice.js
@@ -0,0 +1,22 @@
+//not api slice, but a traditional slice for redux.
+import { createSlice } from '@reduxjs/toolkit'
+
+const authSlice = createSlice({
+    name: 'auth',
+    initialState: { token: null },
+    reducers: {
+        setCredentials: (state, action) => {
+            const { accessToken } = action.payload
+            state.token = accessToken //not state.auth.token because here we are already in the auth slice...
+        },
+        logOut: (state, action) => {
+            state.token = null
+        },
+    }
+})
+
+export const { setCredentials, logOut } = authSlice.actions
+
+export default authSlice.reducer
+
+export const selectCurrentToken = (state) => state.auth.token
