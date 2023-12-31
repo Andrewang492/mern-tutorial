ed6d8ef64289577db9bb8ff00861bd0adc3ff6a5
Andre Wang on his desktop <andrewang87@yahoo.com.au>
2023-12-31 16:19:55 +1100

    fixed refresh problem.

--- frontend/src/app/api/apiSlice.js
+++ frontend/src/app/api/apiSlice.js
@@ -1,5 +1,5 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
-import setCredentials from '../../features/auth/authSlice'
+import { setCredentials } from '../../features/auth/authSlice'
 
 const baseQuery = fetchBaseQuery({ //not only baseUrl field, but two other fields for JWT's.
     baseUrl: 'http://localhost:3500',
@@ -15,7 +15,7 @@ const baseQuery = fetchBaseQuery({ //not only baseUrl field, but two other field
 })
 
 const baseQueryWithReauth = async (args, api, extraOptions) => {
-    console.log(args) // request url, method, body
+    // console.log(args) // request url, method, body
     // console.log(api) // signal, dispatch, getState()
     // console.log(extraOptions) //custom like {shout: true}
 
@@ -23,31 +23,21 @@ const baseQueryWithReauth = async (args, api, extraOptions) => {
 
     // If you want, handle other status codes, too
     if (result?.error?.status === 403) {
-        console.log(result)
+        // console.log(result)
         console.log('sending refresh token because status was 403.')
 
         // send refresh token to get new access token 
         const refreshResult = await baseQuery('/auth/refresh', api, extraOptions)
-        console.log('refresh result')
-        console.log(refreshResult)
         if (refreshResult?.data) {
-            console.log('refresh result has data (ie probably accessToken)')
-            console.log({ accessToken: refreshResult.data.accessToken })
 
             // store the new token 
-            api.dispatch(setCredentials({ accessToken: refreshResult.data.accessToken }))
+            api.dispatch(setCredentials({ ...refreshResult.data }))
 
             // retry original query with new access token
-            console.log('refetching query results using new access token')
-            try {
-                result = await baseQuery(args, api, extraOptions).unwrap()
-                console.log(result)
-            } catch (error) {
-                console.error(error)
-            }
+            result = await baseQuery(args, api, extraOptions)
             
         } else {
-            console.log('refresh result has no data, ie couldnt refresh the access token')
+            // console.log('refresh result has no data, ie couldnt refresh the access token')
             if (refreshResult?.error?.status === 403) {
                 refreshResult.error.data.message = "Your login has expired. "
                 // i.e refresh did not work.
