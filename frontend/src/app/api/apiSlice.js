6d5373f579cc9da2e163a132481e0d93665c7627
Andre Wang on his desktop <andrewang87@yahoo.com.au>
2024-01-01 19:24:22 +1100

    changed baseUrl for frontend callling backend, and allowed origins. These depended on what render.com gave me.

--- frontend/src/app/api/apiSlice.js
+++ frontend/src/app/api/apiSlice.js
@@ -2,7 +2,8 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 import { setCredentials } from '../../features/auth/authSlice'
 
 const baseQuery = fetchBaseQuery({ //not only baseUrl field, but two other fields for JWT's.
-    baseUrl: 'https://technotes-api.onrender.com', //use https instead of http
+    // baseUrl: 'https://technotes-api.onrender.com',
+    baseUrl: 'https://technotes-api-ohmk.onrender.com',
     credentials: 'include', //will always send cookie. Now with this, navigating to get notes will use the cookie to get notes.
     prepareHeaders: (headers, { getState }) => {
         const token = getState().auth.token
--- lesson03/config/allowedOrigins.js
+++ lesson03/config/allowedOrigins.js
@@ -1,6 +1,7 @@
 const allowedOrigins = [
     'https://technotes.onrender.com',
-    'http://technotes.onrender.com'
+    'http://technotes.onrender.com',
+    'https://technotes-t0ih.onrender.com'
 ]
 
 module.exports = allowedOrigins
